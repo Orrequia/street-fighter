c05d8672a752b2206d4deafdce743530c1b580d9
francisco.orrequia <francisco.orrequia@aktios.com>
2021-04-16 11:50:10 +0200

    Structure ok

---
+++ src/main/java/com/restapisample/Solution.java
@@ -0,0 +1,9 @@
+package com.restapisample;
+
+import java.util.*;
+
+public class Solution {
+    public static String[] superStreetFighterize(String[][] fighters, int[] position, String[] moves) {
+        return new String[]{};
+    }
+}
